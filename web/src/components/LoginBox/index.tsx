2292bf684df6a20937e2b72c072e3a533919f150
williansamer <willian.samer@gmail.com>
2022-03-11 08:06:48 -0300

    LoginBox ok

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,5 +1,15 @@
+
+import { VscGithubInverted } from "react-icons/vsc"
+import styles from "./styles.module.scss";
+
 export function LoginBox() {
   return (
-    <h1>LoginBox</h1>
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong> 
+      <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" /> {/* Com isso, ele se torna um svg */}
+        Entrar com GitHub
+      </a>
+    </div>
   )
 }
